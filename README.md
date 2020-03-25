7be5300949a1adb038a9671bde281f4f9a99d978
James <40815140+J-M1@users.noreply.github.com>
2020-03-25 12:21:44 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,38 @@
+# Captcha-Solver
+This is a Recaptcha V3 Harvester you can use for any site, just replace the sitekey and website you wish to use.
+This solver automatically removes expired captcha tokens after they expire (120 seconds).
+
+# Using the tokens
+
+You can use this request in a function to return a captcha token, which can then be used. 
+Retrieving a token will automatically remove it from the bank.
+
+```
+request('http://localhost:8080/fetch', function (error, response, body) {
+            if(body != undefined)
+            {
+                var tokenPot = JSON.parse(body)
+                if(body == "[]")
+                {
+                    console.log("Waiting for captcha...")
+                    setTimeout(getCaptchaToken, 500, sizeID);
+                }
+                else
+                {
+                    console.log("Got captcha token...")
+                    captchaToken = tokenPot[0]["token"]
+                    return captchaToken;
+                }
+            }
+            else if(body == undefined)
+            {
+                console.log("Captcha bank not active.")
+            }
+        });
+```
+
+# Instructions
+Clone this repo, then:
+```npm install``` 
+then
+``` npm start```
---
+++ main.js
@@ -0,0 +1,127 @@
+const electron = require('electron')
+const { app, BrowserWindow, protocol, net } = electron;
+const fs = require('fs');
+const moment = require('moment');
+const express = require('express')
+const bodyParser = require('body-parser')
+
+var expressApp, bankExpressApp, bankServer;
+
+var captchaBank = [];
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+app.on('ready', () => {
+	initCaptchaWindow();
+})
+
+async function initCaptchaWindow() {
+	captchaWindow = new BrowserWindow({
+		width: 480,
+		height: 680,
+			webPreferences: {
+				allowRunningInsecureContent: true
+			}
+	})
+
+	SetupIntercept();
+
+	captchaWindow.loadURL('https://accounts.google.com');
+	
+	await sleep(1000)
+	
+	captchaWindow.on('close', function(e){
+		captchaWindow = null;
+	});
+
+	captchaWindow.webContents.session.webRequest.onBeforeRequest({urls: ['https://myaccount.google.com/*']}, (details, callback) => {
+		callback({redirectURL: 'http://www.supplystore.com.au/'})
+	})
+};
+
+function SetupIntercept() {
+	protocol.interceptBufferProtocol('http', (req, callback) => {
+		if(req.url == 'http://www.supplystore.com.au/') {
+			fs.readFile(__dirname + '/captcha.html', 'utf8', function(err, html){
+				callback({mimeType: 'text/html', data: Buffer.from(html)});
+			});
+		}else{
+			const request = net.request(req)
+			request.on('response', res => {
+				const chunks = []
+	
+				res.on('data', chunk => {
+					chunks.push(Buffer.from(chunk))
+				})
+	
+				res.on('end', async () => {
+					const file = Buffer.concat(chunks)
+					callback(file)
+				})
+			})
+	
+			if (req.uploadData) {
+				req.uploadData.forEach(part => {
+					if (part.bytes) {
+						request.write(part.bytes)
+					} else if (part.file) {
+						request.write(readFileSync(part.file))
+					}
+				})
+			}
+	
+			request.end()
+		}
+	})
+};
+
+electron.ipcMain.on('openCapWindow', function(event, args) {
+    initCaptchaWindow();
+});
+
+electron.ipcMain.on('sendCaptcha', function(event, token) {
+	captchaBank.push({
+	  token: token,	
+	  timestamp: moment(),
+	  host: 'http://www.supplystore.com.au/',
+	  sitekey: '6LfucpUUAAAAAC1oYq1gsmm2MXk8TjrH_mkcLeL8'
+	})
+});
+
+setInterval(function(){
+	for (var i = 0; i < captchaBank.length; i++) {
+
+  	if (moment().diff(moment(captchaBank[i].timestamp), 'seconds') > 110) {
+		//console.log('Removing Expired Captcha Token')
+		captchaBank.splice(0,1)
+		
+	  }
+	}
+}, 1000);
+
+function initBankServer() {
+	bankExpressApp = express()
+
+	let port = '8080';
+
+	console.log('Bank server listening on port: ' + port);
+	bankExpressApp.set('port', port);
+	bankExpressApp.use(bodyParser.json());
+	bankExpressApp.use(bodyParser.urlencoded({ extended: true }));
+
+	bankExpressApp.get('/trigger', function(req, res) {
+		initCaptchaWindow();
+	});
+
+	bankExpressApp.get('/fetch', function(req, res) {
+		return res.json(captchaBank),
+		captchaBank.splice(0,1);
+	});
+
+	bankServer = bankExpressApp.listen(bankExpressApp.get('port'));
+
+	}
+  
+initBankServer();
